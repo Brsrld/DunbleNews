5fde0c04917394f37ba7f0ce26d42407de624106
Barış ŞARALDI <brsrld@gmail.com>
2023-03-14 14:08:47 +0300

    Initial Commit

---
+++ DunbleNews/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DunbleNews
+//
+//  Created by Barış ŞARALDI on 14.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DunbleNews/DunbleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DunbleNewsApp.swift
+//  DunbleNews
+//
+//  Created by Barış ŞARALDI on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DunbleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
